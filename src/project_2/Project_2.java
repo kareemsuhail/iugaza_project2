1b9e9525059b931f34462075228736192dd96927
kareemsuhail <1kayesh@students.iugaza.edu.ps>
2016-12-05 20:10:00 +0200

    =solving input issue

--- src/project_2/Project_2.java
+++ src/project_2/Project_2.java
@@ -5,7 +5,7 @@
  */
 package project_2;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import java.util.Scanner;
 
 /**
  *
@@ -17,20 +17,92 @@ public class Project_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        double[][] x = {{1,5,5},{4,6,5},{3,5,4}} ;
-        double[][] b = {{1,2},{1,2},{1,2}} ;
-        double[][] d = multiplyMatrix(b,x) ;
-        for (int i = 0; i < d.length ; i++) {
-            for (int j = 0; j < d[0].length ; j++) {
-                System.out.print(d[i][j] + " ");
-            }
-            System.out.println();
+          int numOfqueries = 0 ;
+          String[] queries ;
+          Scanner inputQuery ;
+          Scanner inputMatrixB ;
+          Scanner inputDimensionB ;
+          Scanner num;
+          Scanner inputMatrixA ;
+          Scanner inputDimensionA;
+          Scanner numTest = new Scanner(System.in);
+          System.out.println("Enter the number of test cases");
+          int numOfTestCases = numTest.nextInt();
+          ArrayCustomized[] arrays = new ArrayCustomized[numOfTestCases];
+          for (int i = 1; i <= numOfTestCases ; i++) {
+              arrays[i-1] = new ArrayCustomized();
+              System.out.println("Test case "+ i);
+              inputDimensionA = new Scanner(System.in);
+              System.out.println("enter the dimension of Matrix A :");
+              int dimension1 = inputDimensionA.nextInt();
+              int dimension2 = inputDimensionA.nextInt();
+               inputMatrixA = new Scanner(System.in);
+              System.out.println("enter the value of rows and colunms of matrix A");
+              double[][] MatrixA = new double[dimension1][dimension2];
+              for (int j = 0; j < dimension2; j++) {
+                  for (int k = 0; k < dimension1; k++) {
+                      MatrixA[k][j] = inputMatrixA.nextInt() ;
+                  }
+              }
+              arrays[i-1].setA(MatrixA);
+              
+                inputDimensionB = new Scanner(System.in);
+              System.out.println("enter the dimension of Matrix B :");
+              int dimension11 = inputDimensionB.nextInt();
+              int dimension22 = inputDimensionB.nextInt();
+               System.out.println("enter the value of rows and colunms of matrix B");
+               inputMatrixB = new Scanner(System.in);
+              double[][] MatrixB = new double[dimension11][dimension22];
+              for (int j = 0; j < dimension22; j++) {
+                  for (int k = 0; k < dimension11; k++) {
+                      MatrixB[k][j] = inputMatrixB.nextInt() ;
+                  }
+              }
+              arrays[i-1].setB(MatrixB);
+               num = new Scanner(System.in);
+              System.out.print("enter the number of queries ");
+              numOfqueries = num.nextInt();
+              System.out.println(numOfqueries);
+               
+              queries = new String[numOfqueries];
+              System.out.println("enter queries");
+              inputQuery = new Scanner(System.in);
+              for (int K = 0; K < numOfqueries; K++) {
+                  queries[K]  = inputQuery.nextLine() ;
+              }
+              arrays[i-1].setQueries(queries);
+               numOfqueries = 0 ;
+           queries  = null;
+           inputQuery  = null;
+           inputMatrixB = null ;
+           inputDimensionB = null;
+           num = null;
+           inputMatrixA = null ;
+           inputDimensionA = null;
+              
+        }
+          for (int i = 0; i < numOfTestCases; i++) {
+              System.out.println("case #" + i);
+              System.out.println("---------------");
+              double[][] temp1 ;
+              for (int j = 0; j < arrays[i].getQueries().length; j++) {
+                   String temp = arrays[i].getQueries()[j];
+                   temp1 = arrays[i].work(temp);
+                   System.out.println(temp);
+                   for (int k = 0; k < temp1.length; k++) {
+                       for (int l = 0; l < temp1[0].length; l++) {
+                           System.out.print(temp1[k][l]);
+                       }
+                       System.out.println();
+                       
+                  }
+              }
+        }
         }
     }
     
     
-}
+
 class ArrayCustomized {
     static int numberOfObjects ;
     private double[][] A ;
@@ -50,23 +122,24 @@ class ArrayCustomized {
         return numOfqueries;
     }
      public double[][] work(String s ){
-         if(s.equals("Transpose B")){
+         if(s.equalsIgnoreCase("Transpose B")){
           return transpos(B);
          
-         }else if(s.equals("Transpose A")){
+         }else if(s.equalsIgnoreCase("Transpose A")){
          return transpos(B);
          }
-         else if(s.equals("SUM A B")|| s.equals("SUM B A")){
+         else if(s.equalsIgnoreCase("SUM A B")|| s.equals("SUM B A")){
          return sum(A, B);
-         }else if (s.equals("SUBT A B")){
+         }else if (s.equalsIgnoreCase("SUBT A B")){
          return sub(A, B);
-         }else if (s.equals("SUBT B A")){
+         }else if (s.equalsIgnoreCase("SUBT B A")){
          return sub(B, A);
-         }else if (s.equals("MUL A B")){
+         }else if (s.equalsIgnoreCase("MUL A B")){
          return mul(A, B);
-         }else if (s.equals("MUL B A")){
+         }else if (s.equalsIgnoreCase("MUL B A")){
          return sub(B,A);
          }
+         return null ;
      
      }
     public String[] getQueries() {
@@ -79,7 +152,7 @@ class ArrayCustomized {
 
     public void setQueries(String[] queries) {
         
-        System.arraycopy(queries, 0, this.queries, 0, queries.length) ;
+       this.queries = queries;
         
     }
     
