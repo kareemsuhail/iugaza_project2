5b71b1d7fffb4ecf0952189ea92c4b8b63864636
kareemsuhail <1kayesh@students.iugaza.edu.ps>
2016-12-14 09:29:30 +0200

    =adding some comments for liscen

--- src/project_2/Project_2.java
+++ src/project_2/Project_2.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ this code has been writen for islamic university as 
+a project for java course ,, 
+writen by : Khaled shurrab and kareem ayesh 
+supervisor : eng.Mustafa daluol 
  */
 package project_2;
 
